08ba0baa950bd38f3557f02037a18adfa4f3a97d
Ngueminping48 <annelaurencekamenikameni@gmail.com>
2024-10-27 23:41:48 +0100

    account.css;account.jsx;footer.css;footer.jsx;home.css créer ce jour.

--- frontend/src/componnents/account/account.jsx
+++ frontend/src/componnents/account/account.jsx
@@ -1,21 +1,20 @@
-import "./account.css"
 
-const Account = () => {
-    return (
+import './account.css';
 
+function Account ({title, amount, description }) {
+return (
+    <section className='account'>
+        <header className='account-content-wrapper'>
+            <h3>{title}</h3>
+            <b>{amount}</b>
+            <p>{description}</p>
+        </header>
+        <div className='account-content-wrapper cta'>
+            <button>View transactions</button>
+        </div>
+    </section>
+)
 
-    )
+}
 
-    }
-        
-       
-
-
-
-
-
-
-
-
-
-export default Account
+export default Account;
