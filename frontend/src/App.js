6de1e342c090253b59cbe2153d8a7169d248950f
Ngueminping48 <annelaurencekamenikameni@gmail.com>
2024-10-28 01:46:59 +0100

    package-lock.json;package.json;App.css;pp.js;App.js;App.test.js;feature.css;index.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,27 @@
-/*.App {
-  text-align: center;
+import React from 'react';
+import './index.css';
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Profile from './pages/profile/Profile'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Nav from './components/nav/Nav';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/profile' element={<Profile />} /> 
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </React.StrictMode>
+
+  );
 }
 
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-
-.App-header {
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}
-
-.App-link {
-  color: #61dafb;
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-*/
+export default App;
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,27 +1,8 @@
-import React from 'react';
-import './index.css';
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Profile from './pages/profile/Profile'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import Nav from './components/nav/Nav';
-import Footer from './components/footer/Footer';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-function App() {
-  return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path='/profile' element={<Profile />} /> 
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </React.StrictMode>
-
-  );
-}
-
-export default App;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
