967a603819dea6193e0b4dd6b6c7a31e0b95f02a
Ngueminping48 <annelaurencekamenikameni@gmail.com>
2024-11-01 13:01:44 +0100

    le code nav.css ; le code Nav.jx Mise à jour du code profiles .css aussi

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,50 +1,46 @@
-import './nav.css'
-import nav_logo from '../../img/argentBankLogo.webp'
-import { logout } from '../../redux/authSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from "react-router-dom"
+import "./nav.css";
+import nav_logo from "../../img/argentBankLogo.webp";
+import { logout } from "../../redux/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const Nav = () => {
-const token = useSelector((state) => state.auth.token);
-const dispatch = useDispatch();
-const userName = useSelector((state) => state.auth.userName);
-const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+  const firstName = useSelector((state) => state.auth.firstName);
+  
+  const navigate = useNavigate();
 
-const handleLogout = () => {
-    dispatch(logout())
-    navigate("/")
-  }
-    return (
-        <nav>
-            <Link to="/">
-                <img
-                src={nav_logo}
-                alt="Argent Bank Logo"
-                />
-                <h1>Argent Bank</h1>
-                </Link>
-                <i className="fa fa-user-circle"></i>
-                {token ? (
-            <div>
-              <Link to="/user">
-                <i className="fa fa-user-circle"></i>
-                {userName}
-              </Link>
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/");
+  };
+  return (
+    <nav>
+      <Link to="/">
+        <img src={nav_logo} alt="Argent Bank Logo" />
+        <h1>Argent Bank</h1>
+      </Link>
+      {token ? (
+        <div className="nav-right">
+          <Link to="/profile">
+          <i className="fa fa-user-circle"></i>
+          {firstName || "[First Name]"} 
+          </Link>
 
-              <Link to="/" onClick={handleLogout}>
-                <i className="fa fa-sign-out"></i>
-                Sign out
-              </Link>
-            </div>
-          ) : (
-            <Link to="/login">
-              <i className="fa fa-user-circle"></i>
-              Sign In
-            </Link>
-          )}
-        </nav>
-    )
-
-}
+          <Link to="/" onClick={handleLogout}>
+            <i className="fa fa-sign-out"></i>
+            Sign out
+          </Link>
+        </div>
+      ) : (
+        <Link to="/login">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </Link>
+      )}
+    </nav>
+  );
+};
 
 export default Nav;
--- frontend/src/pages/profile/index.jsx
+++ frontend/src/pages/profile/index.jsx
@@ -1,133 +1,138 @@
-import './profile.css'
-import Account from '../../components/account/Account';
-import accountData from '../../data/account.json'
-import { useDispatch, useSelector } from 'react-redux';
-import { useState } from 'react';
-import { setUserName } from '../../redux/authSlice';
-import { changeUsername } from '../../services/userServices';
+import "./profile.css";
+import Account from "../../components/account/Account";
+import accountData from "../../data/account.json";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { setUserName } from "../../redux/authSlice";
+import { changeUsername } from "../../services/userServices";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Profile = () => {
-    const dispatch = useDispatch()
-    const user = useSelector((state) => state.auth) || {}
-    const [newUsername, setnewUsername] = useState()
-    const [isEditing, setIsEditing] = useState(false)
-    
-    const handleEditing = () => {
-        setnewUsername(user.userName)
-        setIsEditing(true)
-      }
+  const dispatch = useDispatch();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const user = useSelector((state) => state.auth) || {};
+  const [newUsername, setnewUsername] = useState();
+  const [isEditing, setIsEditing] = useState(false);
+  const navigate = useNavigate();
 
-    const handleInputChange = (e) => {
-    setnewUsername(e.target.value)
+  const handleEditing = () => {
+    setnewUsername(user.userName);
+    setIsEditing(true);
+  };
+
+  const handleInputChange = (e) => {
+    setnewUsername(e.target.value);
+  };
+
+  const handleSaveNewUsername = async () => {
+    if (newUsername.trim().length < 2) {
+      return;
     }
+    try {
+      dispatch(setUserName({ ...user, userName: newUsername }));
+      setIsEditing(false);
 
-    const handleSaveNewUsername = async () => {
-        if (newUsername.trim().length < 2) {
-          return
-        }
-        try {
-          dispatch(setUserName({ ...user, userName: newUsername }))
-          setIsEditing(false)
-    
-          const payload = {
-            userName: newUsername,
-          }
-    
-          await changeUsername(payload, user.token)
-        } catch (error) {
-          console.error("Failed to change username:", error)
-        }
-      }
-    
+      const payload = {
+        userName: newUsername
+      };
 
-    return (
-        <main className='profile bg-dark'>
-            <div className="header">
-      {isEditing ? (
-        <div className="edit-user-info">
-          <h2>Edit user info</h2>
-          <div>
-            <label htmlFor="username">User name:</label>
-            <input
-              id="username"
-              className="edit-user edit-username-input"
-              type="text"
-              value={newUsername !== undefined ? newUsername : user.userName}
-              onChange={handleInputChange}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  handleSaveNewUsername()
-                }
-                if (e.key === "Escape" || e.key === "ESC") {
-                  setIsEditing(false)
-                }
-              }}
-              autoFocus
-            />
-          </div>
-          <div>
-            <label htmlFor="firstName">First name:</label>
-            <input
-              id="firstName"
-              className="edit-user --not-editable"
-              type="text"
-              value={user.firstName || "[First Name]"}
-              readOnly
-            />
-          </div>
-          <div>
-            <label htmlFor="lastName">Last name:</label>
-            <input
-              id="lastName"
-              className="edit-user --not-editable"
-              type="text"
-              value={user.lastName || "[Last Name]"}
-              readOnly
-            />
-          </div>
-        </div>
-      ) : (
-        <h1>
-          Welcome back <br />
-          {`${user.userName || "[Username]"} `} !
-        </h1>
-      )}
+      await changeUsername(payload, user.token);
+    } catch (error) {
+      console.error("Failed to change username:", error);
+    }
+  };
+  useEffect(() => {
+    if (!user.token) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
 
-      {isEditing ? (
-        <div className="edit-buttons">
-          <button
-            className="edit-button-on"
-            onClick={handleSaveNewUsername}>
-            Save Name
+  return (
+    <main className="profile bg-dark">
+      <div className="header">
+        {isEditing ? (
+          <div className="edit-user-info">
+            <h2>Edit user info</h2>
+            <div>
+              <label htmlFor="username">User name:</label>
+              <input
+                id="username"
+                className="edit-user edit-username-input"
+                type="text"
+                value={newUsername !== undefined ? newUsername : user.userName}
+                onChange={handleInputChange}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    handleSaveNewUsername();
+                  }
+                  if (e.key === "Escape" || e.key === "ESC") {
+                    setIsEditing(false);
+                  }
+                }}
+                autoFocus
+              />
+            </div>
+            <div>
+              <label htmlFor="firstName">First name:</label>
+              <input
+                id="firstName"
+                className="edit-user --not-editable"
+                type="text"
+                value={user.firstName || "[First Name]"}
+                readOnly
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last name:</label>
+              <input
+                id="lastName"
+                className="edit-user --not-editable"
+                type="text"
+                value={user.lastName || "[Last Name]"}
+                readOnly
+              />
+            </div>
+          </div>
+        ) : (
+          <h1>
+            Welcome back <br />
+            {`${user.firstName || "[First Name]"} ${
+              user.lastName || "[Last Name]"
+            }`}{" "}
+            !
+          </h1>
+        )}
+
+        {isEditing ? (
+          <div className="edit-buttons">
+            <button className="edit-button-on" onClick={handleSaveNewUsername}>
+              Save Name
+            </button>
+            <button
+              className="edit-button-on"
+              onClick={() => setIsEditing(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <button className="edit-button" onClick={handleEditing}>
+            Edit Name
           </button>
-          <button
-            className="edit-button-on"
-            onClick={() => setIsEditing(false)}>
-            Cancel
-          </button>
-        </div>
-      ) : (
-        <button className="edit-button" onClick={handleEditing}>
-          Edit Name
-        </button>
-      )}
-    </div>
+        )}
+      </div>
 
-            {accountData.map((account, index) => (
-                <Account
-                    key={index}
-                    title={account.title}
-                    amount={account.amount}
-                    description={account.description}
-                />
-            ))}
-        </main>
-
-
-
-
-    )
-
+      {accountData.map((account, index) => (
+        <Account
+          key={index}
+          title={account.title}
+          amount={account.amount}
+          description={account.description}
+        />
+      ))}
+    </main>
+  );
 };
 
 export default Profile;
